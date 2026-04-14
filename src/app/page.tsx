53ef2523030b6396f28cdc8ee85f0ad21dc51211
Ankamo <102838840+Ankamo@users.noreply.github.com>
2026-04-13 21:27:00 -0500

    Actualizar page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,10 @@
+import Nav from "@/components/Nav";
+
 export default function Home() {
   return (
     <>
       <header>
-        <nav style={{ backgroundColor: "#333", padding: "1rem" }}>
-          <ul style={{ display: "flex", listStyle: "none", margin: 0, padding: 0 }}>
-            <li style={{ marginRight: "1rem" }}>
-              <a href="#section1" style={{ color: "#fff", textDecoration: "none" }}>
-                Inicio
-              </a>
-            </li>
-            <li style={{ marginRight: "1rem" }}>
-              <a href="#section2" style={{ color: "#fff", textDecoration: "none" }}>
-                Sobre Nosotros
-              </a>
-            </li>
-            <li>
-              <a href="#section3" style={{ color: "#fff", textDecoration: "none" }}>
-                Contacto
-              </a>
-            </li>
-          </ul>
-        </nav>
+        <Nav />
       </header>
 
       <main>
