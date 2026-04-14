a9bbf35083a6ad9298946e814af842898883bb6b
Ankamo <102838840+Ankamo@users.noreply.github.com>
2026-04-13 21:30:12 -0500

    Actualizar el archivo Nav.tsx con nuevos elementos en el menú

---
+++ Nav.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Inicio</li>
+        <li>Institución</li>
+        <li>Cursos y Diplomados</li>
+        <li>Inscripciones</li>
+        <li>Campus Virtual</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
