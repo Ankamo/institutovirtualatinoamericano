cc6c08005f5e6557742d41e6b975de178911c7aa
Ankamo <102838840+Ankamo@users.noreply.github.com>
2026-04-13 21:24:58 -0500

    Crear el componente Nav.tsx

---
+++ src/components/Nav.tsx
@@ -0,0 +1,25 @@
+const Nav = () => {
+  return (
+    <nav style={{ backgroundColor: "#333", padding: "1rem" }}>
+      <ul style={{ display: "flex", listStyle: "none", margin: 0, padding: 0 }}>
+        <li style={{ marginRight: "1rem" }}>
+          <a href="#section1" style={{ color: "#fff", textDecoration: "none" }}>
+            Inicio
+          </a>
+        </li>
+        <li style={{ marginRight: "1rem" }}>
+          <a href="#section2" style={{ color: "#fff", textDecoration: "none" }}>
+            Sobre Nosotros
+          </a>
+        </li>
+        <li>
+          <a href="#section3" style={{ color: "#fff", textDecoration: "none" }}>
+            Contacto
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
